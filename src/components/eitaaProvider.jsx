ffababe52a06e4852e67d9b12511cc61b564a8bb
Mohammad Pourhosein <m.pourhosein7777gmail.com>
2025-11-20 14:39:26 +0330

    a

--- src/components/eitaaProvider.jsx
+++ src/components/eitaaProvider.jsx
@@ -18,29 +18,32 @@ export default function EitaaProvider() {
     webApp.isVerticalSwipesEnabled = false;
     webApp.setHeaderColor("#155DFD");
 
+    // تغییر آیکون BackButton
+    if (webApp.BackButton) {
+      if (isHome) {
+        webApp.enableClosingConfirmation();
+        webApp.BackButton.onClick(() => {
+          webApp.showConfirm("آیا می‌خواهید خارج شوید؟", (confirmed) => {
+            if (confirmed) webApp.close();
+          });
+        });
+      } else {
+        webApp.BackButton.onClick(() => {
+          router.back();
+        });
+        webApp.disableClosingConfirmation?.();
+      }
+    }
+
     // Fullscreen فقط در موبایل
     if (["android", "ios", "mobile_web"].includes(webApp.platform)) {
       webApp.requestFullscreen();
     }
 
-    if (isHome) {
-      // صفحه اصلی → BackButton مخفی، MainButton با آیکون close
-      // webApp.BackButton.hide();
-      webApp.enableClosingConfirmation();
-
-      webApp.BackButton.onClick(() => {
-        // webApp.showConfirm("آیا می‌خواهید خارج شوید؟", (confirmed) => {
-        //   if (confirmed) webApp.close();
-        // });
-      });
-    } else {
-      disableClosingConfirmation();
-      router.back();
-    }
-
     // cleanup
     return () => {
       webApp.MainButton.offClick();
+      webApp.BackButton?.offClick();
     };
   }, [pathname, router]);
 
